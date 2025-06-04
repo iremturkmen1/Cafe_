53bcb37be4f65fab3ebf0d3e8d89ad82aeeae0ba
iremturkmen1 <iremturkmen@ogrenci.beykoz.edu.tr>
2025-06-04 22:02:04 +0300

    Add project files.

---
+++ CafeRestaurant_/Migrations/20250527133940_AddInventoryCategory.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CafeRestaurant_.Migrations
+{
+    public partial class AddInventoryCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "InventoryCategoryId",
+                table: "Envanters",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "InventoryCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InventoryCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Envanters_InventoryCategoryId",
+                table: "Envanters",
+                column: "InventoryCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Envanters_InventoryCategories_InventoryCategoryId",
+                table: "Envanters",
+                column: "InventoryCategoryId",
+                principalTable: "InventoryCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Envanters_InventoryCategories_InventoryCategoryId",
+                table: "Envanters");
+
+            migrationBuilder.DropTable(
+                name: "InventoryCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Envanters_InventoryCategoryId",
+                table: "Envanters");
+
+            migrationBuilder.DropColumn(
+                name: "InventoryCategoryId",
+                table: "Envanters");
+        }
+    }
+}
---
+++ CafeRestaurant_/Migrations/20250527151603_RemoveCategoryIdFromEnvanter.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CafeRestaurant_.Migrations
+{
+    public partial class RemoveCategoryIdFromEnvanter : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Envanters_Categories_CategoryId",
+                table: "Envanters");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Envanters_InventoryCategories_InventoryCategoryId",
+                table: "Envanters");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Envanters_CategoryId",
+                table: "Envanters");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Envanters");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InventoryCategoryId",
+                table: "Envanters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Envanters_InventoryCategories_InventoryCategoryId",
+                table: "Envanters",
+                column: "InventoryCategoryId",
+                principalTable: "InventoryCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Envanters_InventoryCategories_InventoryCategoryId",
+                table: "Envanters");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InventoryCategoryId",
+                table: "Envanters",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Envanters",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Envanters_CategoryId",
+                table: "Envanters",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Envanters_Categories_CategoryId",
+                table: "Envanters",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Envanters_InventoryCategories_InventoryCategoryId",
+                table: "Envanters",
+                column: "InventoryCategoryId",
+                principalTable: "InventoryCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
